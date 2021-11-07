a223ed15919e3750615f2f3f5b55e9283e96dbc1
antonturch <antonturch@mail.ru>
2021-11-07 21:20:43 +0300

    Lesson 12: storybook, decorator, snapshot testing(baga)

---
+++ src/ReduxStoreProviderDecorator.tsx
@@ -0,0 +1,35 @@
+import {Provider} from "react-redux";
+import {store} from "./state/store";
+
+// const TodolistID1 = v1();
+// const TodolistID2 = v1();
+//
+// const rootReducers = combineReducers({
+//     todolists: todolistReducer,
+//     tasks: taskReducer
+// })
+
+// const initGlobalState = {
+//     todolists: [
+//         {id: TodolistID1, title: "What to learn", filter: "all"},
+//         {id: TodolistID2, title: "What to buy", filter: "all"},
+//     ],
+//     tasks: {
+//         [TodolistID1]: [{id: v1(), title: "HTML", isDone: true,},
+//             {id: v1(), title: "CSS", isDone: false,},
+//             {id: v1(), title: "JS", isDone: false,},],
+//         [TodolistID2]: [{id: v1(), title: "React", isDone: true,},
+//             {id: v1(), title: "Material UI", isDone: false,},
+//             {id: v1(), title: "Redux", isDone: false,},],
+//     },
+// }
+//
+// const storeForStorybook = createStore(rootReducers, initGlobalState as AppRootStateType)
+
+export const ReduxStoreProviderDecorator = (storyFn: any) => {
+  return (
+      <Provider store={store}>
+        {storyFn()}
+      </Provider>
+  )
+}
--- src/stories/AppWithRedux.stories.tsx
+++ src/stories/AppWithRedux.stories.tsx
@@ -1,16 +1,14 @@
 import AppWithReducers from "../AppWithReducers";
-import {Provider} from "react-redux";
-import {store} from "../state/store";
+import {ReduxStoreProviderDecorator} from "../ReduxStoreProviderDecorator";
 
 export default {
     title: "AppWithRedux",
     component: AppWithReducers,
+    decorators: [ReduxStoreProviderDecorator]
 }
 
 export const AppWithReducersStory = () => {
     return (
-        <Provider store={store}>
-            <AppWithReducers/>
-        </Provider>
+        <AppWithReducers/>
     )
 }
